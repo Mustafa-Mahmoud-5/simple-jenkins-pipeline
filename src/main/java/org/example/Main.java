07e686077a4f60298ac57572b9edbabdc52a0a65
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2025-12-12 02:50:39 +0200

    test: testing the added jenkins webhook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Simple Jenkins Demo Project");
+        System.out.println("testing the webhook...");
     }
 }
